4913e5d9782295646cfa3368d0072feb9c65db8f
wylenjoan <57890826+wylenjoan@users.noreply.github.com>
2021-10-18 21:07:53 +0800

    Add project files

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "1234",
+    database: "face-recognition-brain-db",
+  },
+});
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  db.select("*")
+    .table("users")
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+app.post("/signin", (req, res) => signin.handleSignin(req, res, db, bcrypt));
+
+app.post("/register", (req, res) =>
+  register.handleRegister(req, res, db, bcrypt)
+);
+
+app.get("/profile/:id", (req, res) => profile.handleProfile(req, res, db));
+
+app.put("/image", (req, res) => image.handleImage(req, res, db));
+app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
